d5f746e56db5d1dc1bd39a3247024383959eb076
Hendi Santika <hendisantika@gmail.com>
2018-03-18 19:43:33 +0700

    #17 - Modify Main App

--- src/main/java/com/hendisantika/springbootjwtdemo/SpringBootJwtDemoApplication.java
+++ src/main/java/com/hendisantika/springbootjwtdemo/SpringBootJwtDemoApplication.java
@@ -1,7 +1,10 @@
 package com.hendisantika.springbootjwtdemo;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.orbitz.consul.Consul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootJwtDemoApplication {
@@ -9,4 +12,14 @@ public class SpringBootJwtDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJwtDemoApplication.class, args);
 	}
+
+    @Bean
+    Consul getConsulClient() {
+        return Consul.builder().build();
+    }
+
+    @Bean
+    ObjectMapper getObjectMapper() {
+        return new ObjectMapper();
+    }
 }
