82773717377035824a505871dba5b605e070a1de
Hendi Santika <hendisantika@gmail.com>
2019-12-15 19:00:22 +0700

    #35 - Upgraded into Spring Boot 2.2.1.RELEASE version

--- src/main/java/com/hendisantika/springbootjwtdemo/config/WebSecurityConfig.java
+++ src/main/java/com/hendisantika/springbootjwtdemo/config/WebSecurityConfig.java
@@ -4,9 +4,7 @@ import com.hendisantika.springbootjwtdemo.filter.ExceptionHandlerFilter;
 import com.hendisantika.springbootjwtdemo.filter.JwtAuthenticationFilter;
 import com.hendisantika.springbootjwtdemo.filter.JwtLoginFilter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -27,7 +25,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+//@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
